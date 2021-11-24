812f52d64f5bc9c130be973b32e62d670df88237
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-11-24 17:49:29 +0900

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,1 +1,10 @@
+"""
+SensiMix: Sensitivity-Aware 8-bit Index & 1-bit Value Mixed Precision Quantization for BERT Compression
+Authors:
+- Tairen Piao (piaotairen@snu.ac.kr), Seoul National University
+- Ikhyun Cho (ikhyuncho@snu.ac.kr), Seoul National University
+- U Kang (ukang@snu.ac.kr), Seoul National University
+This software may be used only for research evaluation purposes.
+For other purposes (e.g., commercial), please contact the authors.
+"""
 cd script; bash ft.sh
