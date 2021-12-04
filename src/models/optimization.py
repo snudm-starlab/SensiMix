3dd63e40c1301683521081bd6177db585909fd64
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-12-04 09:16:30 +0900

    Update optimization.py

--- src/models/optimization.py
+++ src/models/optimization.py
@@ -1,5 +1,4 @@
-"""
-SensiMix: Sensitivity-Aware 8-bit Index & 1-bit Value Mixed Precision Quantization for BERT Compression
+"""SensiMix: Sensitivity-Aware 8-bit Index & 1-bit Value Mixed Precision Quantization for BERT Compression
 Authors:
 - Tairen Piao (piaotairen@snu.ac.kr), Seoul National University
 - Ikhyun Cho (ikhyuncho@snu.ac.kr), Seoul National University
@@ -7,6 +6,7 @@ Authors:
 This software may be used only for research evaluation purposes.
 For other purposes (e.g., commercial), please contact the authors.
 """
+
 """PyTorch optimization for BERT model."""
 
 import logging
